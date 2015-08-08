c4359eb5ba04410dc1ff5e5e25a35197f0d8fc1b
kurehajime <x.iidec+gh@googlemail.com>
2015-08-08 21:15:01 +0900

    add model

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,9 @@
 from google.appengine.ext import ndb
 
 # Create your models here.
+class Facility(db.Model):
+	fid = ndb.IntegerProperty()
+	name = ndb.StringProperty()
+	description=ndb.StringProperty(multiline=True)
+	geo=ddb.GeoPtProperty()
+	date=ndb.DateTimeProperty(auto_now_add=True)
